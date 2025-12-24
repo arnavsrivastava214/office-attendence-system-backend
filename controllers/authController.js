1ea3a30c613e17cb3da1514611fc0ba31c090efc
Arnav Srivastava <arnavsrivastava214@gmail.com>
2025-12-24 23:54:37 +0530

    debug

--- controllers/authController.js
+++ controllers/authController.js
@@ -187,7 +187,7 @@ exports.register = async (req, res) => {
     );
   } catch (err) {
     console.error(err);
-    res.status(500).json({ error: 'Registration failed' });
+    res.status(500).json({ error: 'Registration failed', err:err });
   }
 };
 
